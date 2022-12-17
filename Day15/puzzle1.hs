ae7582c298da149a4cef09c13270a0c672ad3bfc
FlowerBoy-SkullGirl <dmurphyshort@gmail.com>
2022-12-17 00:25:47 -0500

    Add: Solution for part 2 that runs in okay time. Still takes quite a while probably at findHoleBetweenxk because it can potetially iterate 	4000000 items

--- Day15/puzzle1.hs
+++ Day15/puzzle1.hs
@@ -27,26 +27,35 @@ canSeeAtY y (b,s) = if differenceY == 0 then [] else allBetween maxWest maxEast 
     differenceY = if (range - manhattan) >= 0 then (range - manhattan) else 0
     manhattan = snd $ distancesCoords (h,v) (h,y)
 
-canSeeRange :: Int -> ((Coord,Int),(Coord)) -> Set.Set Int
-canSeeRange y (b,s) = Set.fromAscList [maxWest..maxEast] where
+--If can't reach y, return the origin
+canSeeRange :: Int -> ((Coord,Int),(Coord)) -> (Int,Int)
+canSeeRange y (b,s) = if differenceY == 0 then (0,0) else (maxWest,maxEast) where
     h = fst (fst b)
     v = snd (fst b)
     range = snd b
     maxWest = h - differenceY
     maxEast = h + differenceY
-    differenceY = range - manhattan
+    differenceY = if (range - manhattan) >= 0 then (range - manhattan) else 0
     manhattan = snd $ distancesCoords (h,v) (h,y)
 
 canSeeWithinXandKatY :: Int -> Int -> Int -> [((Coord,Int),Coord)] -> Bool
-canSeeWithinXandKatY x k y bsList = if Set.isSubsetOf (Set.fromAscList [x..k]) uSets then True else False where
-    uSets = unionListOfSets (map canSeeRangehelper bsList)
+canSeeWithinXandKatY x k y bsList = if holeRanges x k (filter (\h -> (fst h) /= (snd h)) ranges) then False else True where
+    ranges = (map canSeeRangehelper bsList)
     canSeeRangehelper z = canSeeRange y z
 
-unionListOfSets :: [Set.Set Int] -> Set.Set Int
-unionListOfSets [] = Set.empty
-unionListOfSets (last:[]) = last
-unionListOfSets sl = Set.union (head sl) sl' where
-    sl' = unionListOfSets (drop 1 sl)
+holeRanges :: Int -> Int -> [(Int,Int)] -> Bool
+holeRanges x k ranges
+    | x < (head $ sort $ map fst ranges) = True
+    | k > (last $ sort $ map snd ranges) = True
+    | overlapAll (sort ranges) = False
+    | otherwise = True
+
+overlapAll :: [(Int, Int)] -> Bool
+overlapAll [] = True
+overlapAll (current:[]) = True
+overlapAll (current:next:[]) = if (snd current) >= (fst next) then True else False
+overlapAll (current:ranges) = if (snd current) >= (fst $ head ranges) then overlapBoth else False where
+    overlapBoth = if (snd current) >= (snd $ head ranges) then overlapAll (current:(drop 1 ranges)) else overlapAll ranges
 
 part1 :: Set.Set Coord -> Set.Set Coord -> Set.Set Coord -> Int
 part1 visible blist slist = Set.size $ Set.difference (Set.difference visible blist) slist
@@ -54,12 +63,26 @@ part1 visible blist slist = Set.size $ Set.difference (Set.difference visible bl
 part2 :: Coord -> Int
 part2 beacon = ((fst beacon) * y_full) + (snd beacon)
 
-noMaybes :: Maybe Int -> Int
-noMaybes Nothing = (-1)
-noMaybes (Just x) = x
-
 y_full = 4000000
 y_test = 20
+
+findHoleBetweenxk :: Int -> [((Coord,Int),Coord)] -> Int -> Int -> Int
+findHoleBetweenxk y bsList x k
+    | not $ canSeeWithinXandKatY 0 y_full y bsList = y
+    | otherwise = findHoleBetweenxk (y-1) bsList x k
+
+findNotOverlap :: [(Int, Int)] -> Int
+findNotOverlap [] = 0
+findNotOverlap (current:[]) = (snd current) + 1
+findNotOverlap (current:next:[]) = if (snd current) >= (fst next) then 0 else (snd current) + 1
+findNotOverlap (current:ranges) = if (snd current) >= (fst $ head ranges) then findNotOverlapBoth else (snd current)+1 where
+    findNotOverlapBoth = if (snd current) >= (snd $ head ranges) then findNotOverlap (current:(drop 1 ranges)) else findNotOverlap ranges
+
+findHxY :: Int -> [((Coord,Int),Coord)] -> Int
+findHxY y bsList = findNotOverlap (sort $ filter (\h -> (fst h) /= (snd h)) ranges)  where
+    ranges = (map canSeeRangehelper bsList)
+    canSeeRangehelper z = canSeeRange y z
+
 
 main = do
     input <- readFile "beacons.in"
@@ -69,10 +92,8 @@ main = do
     let visible = concat $ map canSeeAtYMapped bsList where
         canSeeAtYMapped x = canSeeAtY 2000000 x
     let result = part1 (Set.fromList visible) (Set.fromList $ map snd bsList) (Set.fromList $ map fst (map fst bsList))
-    let hBeacon = noMaybes $ elemIndex False $ map canSeeHelper [0..y_full] where
-        canSeeHelper x = canSeeWithinXandKatY 0 y_full x bsList
-    let result2 = [(x,hBeacon) | x <- [0..y_full]] \\ (concat $ map canSeeAtYMapped bsList) where
-        canSeeAtYMapped x = canSeeAtY hBeacon x
-    let final = part2 (head result2)
+    let hBeacon = findHoleBetweenxk y_full bsList 0 y_full
+    let result2 = findHxY hBeacon bsList
+    let final = part2 (result2, hBeacon) 
     print bsList
     print final
