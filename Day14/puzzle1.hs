be031e6340623849426c23cea1eb8d8fccf3d1ed
FlowerBoy-SkullGirl <dmurphyshort@gmail.com>
2022-12-16 19:08:12 -0500

    Complete: Day 14 part 1. Function checkobstacle is slow. Plan to replace with a set instead of list

--- Day14/puzzle1.hs
+++ Day14/puzzle1.hs
@@ -1,6 +1,7 @@
 import Data.List
 import Data.Char
 import Data.List.Split as Sp
+import qualified Data.Set as Set
 
 type Coord = (Int, Int)
 
@@ -21,44 +22,29 @@ allBetween (x,y) (h,v) = [(x',y') | x' <- hor, y' <- ver] where
     hor = if x <= h then [x..h] else [h..x]
     ver = if y <= v then [y..v] else [v..y]
 
-makeGrid :: Int -> [[Int]]
-makeGrid x = replicate x [0 | y <- [0..x]]
-
-addObstacle :: [[Int]] -> Coord -> [[Int]]
-addObstacle grid coord = intsSplit (length grid) [if coord == (x,y) then 1 else (grid!!y)!!x | y <- [0..((length grid)-1)], x <- [0..((length (grid!!y))-1)]]
-
-intsSplit :: Int -> [Int] -> [[Int]]
-intsSplit _ [] = []
-intsSplit n xs = (x:(intsSplit n xs')) where
-    x = take n xs
-    xs' = drop n xs
-
-gridmapper :: [Coord] -> [[Int]] -> [[Int]]
-gridmapper [] grid = grid
-gridmapper obstacles grid = addObstacle grid' (head obstacles) where
-    grid' = gridmapper (drop 1 obstacles) grid
-
 lowestObstacle :: [Coord] -> Int
 lowestObstacle obstacles = maximum [snd y | y <- obstacles]
 
-newSand :: Int -> [[Int]] -> Int -> Int
-newSand x [] hAbyss = x
+--Subtract one from empty grid pattern because result is reported by previous recursion that adds one after having found solution
+newSand :: Int -> [Coord] -> Int -> Int
+newSand x [] hAbyss = x - 1
 newSand x grid hAbyss = newSand (x+1) grid' hAbyss where
     grid' = moveSand grid hAbyss sandSource
     sandSource = (500,0)
 
 --Y coords are upside down, sand is "falling up." Use North to check below sand
-moveSand :: [[Int]] -> Int -> Coord -> [[Int]]
+moveSand :: [Coord] -> Int -> Coord -> [Coord]
 moveSand grid hA current
     | (snd current) >= hA = []
     | noObs (spotNorth current) = moveSand grid hA (spotNorth current)
     | noObs $ (spotWest . spotNorth) current = moveSand grid hA ((spotWest . spotNorth) current)
     | noObs $ (spotEast . spotNorth) current = moveSand grid hA ((spotEast . spotNorth) current)
-    | otherwise = addObstacle grid current
+    | otherwise = current:grid
     where noObs x = not $ checkObstacle grid x
 
-checkObstacle :: [[Int]] -> Coord -> Bool
-checkObstacle grid (x,y) = if (grid!!y)!!x == 1 then True else False
+checkObstacle :: [Coord] -> Coord -> Bool
+--checkObstacle grid coord = if null [x | x <- grid, x == coord] then False else True
+checkObstacle grid coord = if coord `elem` grid then True else False
 
 spotSouth :: Coord -> Coord
 spotSouth (x,y) = (x,(y-1))
@@ -74,9 +60,9 @@ spotWest (x,y) = ((x-1),y)
 
 
 main = do
-    --input <- readFile "cave.in"
+    input <- readFile "cave.in"
     inputTest <- readFile "cave.test"
-    let obstacles = concat $ map obstacleBuilder $ map parseIn $ lines inputTest
-    let grid = gridmapper obstacles $ makeGrid 1000 where
-    let result = newSand 0 grid (lowestObstacle obstacles)
+    --let obstacles = concat $ map obstacleBuilder $ map parseIn $ lines inputTest
+    let obstacles = concat $ map obstacleBuilder $ map parseIn $ lines input
+    let result = newSand 0 obstacles (lowestObstacle obstacles)
     print result
